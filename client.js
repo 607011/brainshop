edfa575a45d4dd9ad2f1c7f5e5a4ff3c7372babf
Oliver Lau <oliver@von-und-fuer-lau.de>
2013-09-11 07:48:39 +0200

    Erster Check-in.

---
+++ client.js
@@ -0,0 +1,55 @@
+// Copyright (c) 2013 Oliver Lau <ola@ct.de>, Heise Zeitschriften Verlag
+// All rights reserved.
+
+var BS = (function () {
+  var HOST = 'localhost';
+  var PORT = 8000;
+  var URL = 'ws://' + HOST + ':' + PORT + '/';
+  var socket;
+
+  function send(message) {
+    console.log(message);
+    var msg = {
+      type: 'idea',
+      text: message,
+      user: 'ola'
+    };
+    socket.send(JSON.stringify(msg));
+  }
+
+  function sendInput() {
+    send($('#input').val());
+  }
+
+  function openSocket() {
+    socket = new WebSocket(URL);
+    socket.onopen = function () {
+      $('#status').text('connected').addClass('ok');
+    };
+    socket.onerror = function (error) {
+      $('#status').text('connection failed').addClass('error');
+    };
+    socket.onmessage = function (e) {
+      var data = JSON.parse(e.data);
+      $('#board').append($('<div class="message">'
+        + '<span class="idea">' + data.text + '</span>'
+        + '<span class="user">' + data.user + '</span>'
+        + '<span class="date">' + data.date + '</span>'
+        + '</div>'));
+    }
+  }
+
+  return {
+    init: function () {
+      openSocket();
+      $('#submit-button').click(function () {
+        sendInput();
+      });
+    }
+  };
+
+})();
+
+  $(document).ready(function() {
+    BS.init();
+  });
---
+++ server.js
@@ -0,0 +1,25 @@
+// Copyright (c) 2013 Oliver Lau <ola@ct.de>, Heise Zeitschriften Verlag
+// All rights reserved.
+
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({ port: 8000 });
+var users = [];
+var ideas = [];
+
+function sendAll(ws) {
+  for (var i = 0; i < ideas.length; ++i) {
+    ws.send(JSON.stringify(ideas[i]));
+  }
+}
+
+wss.on('connection', function (ws) {
+  users.push(ws);
+  sendAll(ws);
+  ws.on('message', function (message) {
+    var data = JSON.parse(message || '{}');
+    data.date = new Date;
+    ideas.push(data);
+    for (var i = 0; i < users.length; ++i)
+      users[i].send(JSON.stringify(data));
+  });
+});
